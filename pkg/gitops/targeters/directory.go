3e85237d3c6edf932528e8427df895a1b21d0ff5
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2025-09-17 12:44:00 +0200

    Add directory targeter
    Signed-off-by: Tom van Dinther <39470469+tvandinther@users.noreply.github.com>

---
+++ pkg/gitops/targeters/directory.go
@@ -0,0 +1,32 @@
+package targeters
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/tvandinther/gitops-manager/pkg/gitops"
+)
+
+type Directory struct {
+	Prefix        string // The path prefix to use relative to the parent directory for environment directories.
+	DirectoryName string // The name of the parent directory containing the environment directories. Omit or use "" for none.
+	Branch        string // The branch to target the configuration to.
+	Orphan        bool   // Whether to create an orphan branch if branch does not exist. Will take precedence over Upstream if true.
+	Upstream      string // The upstream to base the branch off if orphan is false. Must be set if Orphan is false.
+}
+
+func (d *Directory) CreateTarget(req *gitops.Request) (*gitops.Target, error) {
+	directory := filepath.Join(d.DirectoryName, fmt.Sprintf("%s%s", d.Prefix, req.Environment))
+
+	target := &gitops.Target{
+		Repository: req.TargetRepository,
+		Branch: gitops.TargetBranch{
+			Source:         fmt.Sprintf("next/%s/%s/%s", req.Environment, req.AppName, req.UpdateIdentifier),
+			Target:         fmt.Sprintf("%s", d.Branch),
+			UpstreamSource: d.Upstream,
+		},
+		Directory: directory,
+	}
+
+	return target, nil
+}
